7ccf601fb45de7e0f7c6f508b3d93dfe19239f67
zdat5624 <zdat5624@email.com>
2025-02-07 20:48:02 +0700

    video 53

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,8 +2,11 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -26,8 +29,14 @@ public class UserController {
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("newUser", new User());
         model.addAttribute("hoidanit", "hello from controller with model");
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+        return "hello";
     }
 }
